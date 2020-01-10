f3d719186fde015595f9f9ed3dee94b11ef1de33
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-10 21:10:33 +0200

    count number of zeros and ones in a string using a counter

---
+++ CountNumberOfZerosInString.java
@@ -0,0 +1,25 @@
+ class CountNumberOfZerosInString
+    {
+		static String NumberOfZeros(String s)
+		{
+			int countForZero = 0;
+			int countForOne = 0;
+			for(int i=0; i<s.length(); i++)
+			{
+				if(s.charAt(i)=='0')
+					countForZero++;
+
+				else
+					countForOne++;
+			}
+			return "The string " + s + " has" + " " + countForZero + " zeros" + " and"
+				+ " " + countForOne + " ones";
+		}
+
+        public static void main(String[] args)
+        {
+            String str = "011001110";
+            System.out.printf(NumberOfZeros(str) + "\n");
+
+        }
+	}
